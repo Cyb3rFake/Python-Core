f9f04a2d4b194c0e05c0e1f1c0e099ab8f093242
korvyakov_fn <enemy1987@yandex.ru>
2022-11-07 17:23:29 +0300

    Module "Exceptions & Files" passed

--- 5_Exceptions & Files/47.2_Getting in Shape.py
+++ 5_Exceptions & Files/47.2_Getting in Shape.py
@@ -1,12 +1,35 @@
 """
 Том подтягивался каждый день и записывал свои результаты. Он записывал результаты каждого дня в новую строку, так что каждая строка представляла каждый день, когда он подтягивался.
 Создайте программу, которая принимает n чисел в качестве входных данных и выводит результат n-го дня (начиная с 0).
+
+
+# file = open("D:\\PycharmProjects\\Python-Core\\5_Exceptions & Files\\tst.txt","r")
+# # print(file.read())
+# for i in range(21):
+#     print(file.read(20))
+# file.close()
 """
 
-file = open("/usercode/files/pull_ups.txt")
-n = int(input())
-#your code goes here
-output = file.readlines()[n]
-output = output.replace("\n", "")
-print(output)
-file.close()
+# file = open("D:\\PycharmProjects\\Python-Core\\5_Exceptions & Files\\tst.txt","r")
+# # print(file.read())
+# n = "5"
+# rdlns = file.readlines()
+# cnt = len(rdlns)
+# for i in range(cnt):
+#     if n in rdlns:
+# #         print(f"Day {n}, {rdlns[i]} pull ups ")
+#
+# file.close()
+
+# Том подтягивался каждый день и записывал свои результаты. Он записывал результаты каждого дня в новую строку, так что каждая строка представляла каждый день, когда он подтягивался.
+# Создайте программу, которая принимает n чисел в качестве входных данных и выводит результат n-го дня (начиная с 0).
+# file = open("/usercode/files/pull_ups.txt")
+# n = int(input())
+# #your code goes here
+# output = file.readlines()[n]
+# output = output.replace("\n", "")
+# print(output)
+# file.close()
+
+t = [1,12,123,1234,12345]
+print()
