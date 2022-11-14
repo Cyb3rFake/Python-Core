e698dba93d95f025cbc3df9ddbb910f3b34caf1f
Home2 <enemy1987@yandex.ru>
2022-11-14 20:57:43 +0300

    new task stepik 9.2

--- testFrameOffice.py
+++ testFrameOffice.py
@@ -675,10 +675,3 @@ print(Max[1])
 #     else:
 #         return x + sum_to(x-1)
 # print (sum_to(5))
-
-# def fib(x):
-#   if x == 0 or x == 1:
-#     return 1
-#   else:
-#     return fib(x-1) + fib(x-2)
-# print(fib(4))
