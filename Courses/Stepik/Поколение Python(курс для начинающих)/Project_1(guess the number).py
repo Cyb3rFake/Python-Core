74e46a058a867e09640af7d5bd6ffbd68d3f7cf8
Home2 <enemy1987@yandex.ru>
2022-12-04 22:43:05 +0300

    Project_1(guess the number).py

---
+++ Courses/Stepik/Поколение Python(курс для начинающих)/Project_1(guess the number).py
@@ -0,0 +1,30 @@
+""""Игра угадай число"""
+
+# import random
+
+
+# приглашение к началу игры
+def welcome():
+    usr_name = ''
+    range_ = int(input('Введите границы диапазона угадываемых чисел'))
+    pass
+
+
+# набор фраз для попыток
+def phrases_list():
+    allmost = ['Уже близко', '', '', '']
+    allmost_close = ['Почти угадал', '', '', '']
+    far_awy = ['Пока не рядом', 'Не угадал', '', '']
+
+
+# проверка на число и диапазон
+def check_digit():
+    num ='asd'
+    ch_flag = False
+    ch_flag ==True if num.isdigit()==True else print('Введено не число')
+
+    return ch_flag
+
+# проверка на
+def cheker_num():
+    pass
