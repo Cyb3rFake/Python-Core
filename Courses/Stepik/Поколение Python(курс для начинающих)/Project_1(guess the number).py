e1e1f46301646f2242abd20599fc988bca80d508
korvyakov_fn <enemy1987@yandex.ru>
2022-12-05 17:02:02 +0300

    Project_1 range check finished

--- Courses/Stepik/Поколение Python(курс для начинающих)/Project_1(guess the number).py
+++ Courses/Stepik/Поколение Python(курс для начинающих)/Project_1(guess the number).py
@@ -1,18 +1,20 @@
 """"Игра угадай число"""
-
-# import random
+import random
 
 
 # приглашение к началу игры
 
-test_num = 5
-test_range = (1,10)
-
 def welcome():
     usr_name = ''
-    range_ = int(input('Введите границы диапазона угадываемых чисел'))
-    pass
-
+    while True:
+        try:
+            r1,r2 = map(int,input('Введите через запятую границы диапазона угадываемых чисел: ').split(','))
+            break
+        except ValueError:
+            print('Не верный формат')
+    print(f'Диапазон угадываемых значений установлен от {r1} до {r2}')
+    rnd_num = random.randrange(r1,r2)
+    return str(r1),str(r2),rnd_num
 
 # набор фраз для попыток
 def phrases_list():
@@ -23,13 +25,45 @@ def phrases_list():
 
 # проверка на число и диапазон
 def check_digit():
-    num ='asd'
-    ch_flag = False
-    ch_flag ==True if num.isdigit()==True else print('Введено не число')
+    num = c_num
+    range_ = c_range
+    ch_digit = False
+    ch_range = False
 
-    return ch_flag
+    if num.isdigit() == True:
+        num = 'Введено число'
+        ch_digit = True
+    else:
+        num = 'Введено не число'
+
+    if  range_[0]<=int(c_num)<=range_[1] in range_:
+        range_ = 'Введенное число в диапазоне'
+        ch_range = True
+    else:
+        range_= 'Введенное число вне диапазона'
+
+    return ch_digit,ch_range, num, range_
+
 
 # проверка на
-def cheker_num(num):
+def cheker_num():
+    r1, r2, rnd_num = welcome()
 
-    pass
+    ask_num = int(input('Введите число: '))
+    # print(type(ask_num), ask_num,type(rnd_num),rnd_num)
+    print(ask_num,rnd_num)
+    while rnd_num != ask_num:
+        ask_num = int(input('Не верно, Введите число: '))
+    else:
+        print('Угадал,сука!')
+
+cheker_num()
+
+# a,b,c = welcome()
+# print(a,b,c)
+# a = 5
+# b = int(input())
+# while a!=b:
+#     b = int(input())
+# else:
+#     print('Hui')
