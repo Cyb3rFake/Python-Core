21298184342f6dfb5ce0f4f4df42dc2a3ec85ac6
korvyakov_fn <enemy1987@yandex.ru>
2022-12-16 17:02:04 +0300

    new project and tasks added

--- testFrameOffice.py
+++ testFrameOffice.py
@@ -682,15 +682,19 @@ print(Max[1])
 #   else:
 #     return fib(x-1) + fib(x-2)
 # print(fib(4))
+#
+# class SpecialString:
+#     def __init__(self, cont):
+#         self.cont = cont
+#
+#     def __truediv__(self, other):
+#         line = "=" * len(other.cont)
+#         return "\n".join([self.cont, line, other.cont])
+#
+# spam = SpecialString("spam")
+# hello = SpecialString("Hello world!")
+# print(hello/spam)
 
-class SpecialString:
-    def __init__(self, cont):
-        self.cont = cont
 
-    def __truediv__(self, other):
-        line = "=" * len(other.cont)
-        return "\n".join([self.cont, line, other.cont])
 
-spam = SpecialString("spam")
-hello = SpecialString("Hello world!")
-print(hello/spam)
+print()
