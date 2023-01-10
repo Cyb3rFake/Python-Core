65eb6c2a9cb8b4518bdd156268083fbdd9a8a626
korvyakov_fn <enemy1987@yandex.ru>
2023-01-10 12:26:22 +0300

    P4 2 func unfinished

--- Courses/Stepik/Поколение Python(курс для начинающих)/P4_Caesars_Cipher/Project_4(Caesar_Cipher).py
+++ Courses/Stepik/Поколение Python(курс для начинающих)/P4_Caesars_Cipher/Project_4(Caesar_Cipher).py
@@ -1,9 +1,43 @@
 """Шифр Цезаря"""
 
+symbol_list = '!@#$%^&*,.[]{} '
+eng_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+rus_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+# rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
 
 
+def encrypt(text = 'тест!', k = 1):
+    res = []
+    for i in text:
+        if i in rus_alphabet:
+            s = rus_alphabet.index(i)
+            res.append(rus_alphabet[s - k])
+        elif i in symbol_list:
+            res.append(i)
+    res = ''.join(res)
+    return res
+
+print(encrypt(k=1))
 
 
+def decrypt(text = 'сдрс!', k = 1):
+    res = []
+    for i in text:
+        if i in rus_alphabet:
+            s = rus_alphabet.index(i)
+            if (s + k)>len(rus_alphabet)-1:
+                # print((s + k),len(rus_alphabet)-1)
+                d = (rus_alphabet[(s+k)-len(rus_alphabet)-1])
+                res.append(rus_alphabet[(s+k)-len(rus_alphabet)])
+            else:
+                res.append(rus_alphabet[s + k])
+        elif i in symbol_list:
+            res.append(i)
+    res = ''.join(res)
+    return res
+
+print(decrypt())
 
 
