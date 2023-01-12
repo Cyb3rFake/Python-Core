97cf23d5cc927814baf0535d0b05aa5fd499006b
korvyakov_fn <enemy1987@yandex.ru>
2023-01-12 15:38:41 +0300

    func main added

--- Courses/Stepik/Поколение Python(курс для начинающих)/P4_Caesars_Cipher/Project_4(Caesar_Cipher).py
+++ Courses/Stepik/Поколение Python(курс для начинающих)/P4_Caesars_Cipher/Project_4(Caesar_Cipher).py
@@ -5,31 +5,35 @@ eng_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 rus_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 # rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-
+e_text = "" # зашифрованный текст
+de_text = "" # расшифрованный текст
 
 
 def encrypt(text = 'тест!', k = 1):
+    global e_text
     res = []
+    text = text.lower()
     for i in text:
         if i in rus_alphabet:
             s = rus_alphabet.index(i)
-            res.append(rus_alphabet[s - k])
+            if (s + k)>len(rus_alphabet)-1:
+                print(rus_alphabet[(s+k)-len(rus_alphabet)])
+            else:
+                res.append(rus_alphabet[s + k])
         elif i in symbol_list:
             res.append(i)
-    res = ''.join(res)
-    return res
-
-print(encrypt(k=1))
-
+    e_text = ''.join(res)
+    return e_text
 
 def decrypt(text = 'сдрс!', k = 1):
+    global de_text
     res = []
     for i in text:
         if i in rus_alphabet:
             s = rus_alphabet.index(i)
             if (s + k)>len(rus_alphabet)-1:
                 # print((s + k),len(rus_alphabet)-1)
-                d = (rus_alphabet[(s+k)-len(rus_alphabet)-1])
+                # d = (rus_alphabet[(s+k)-len(rus_alphabet)-1])
                 res.append(rus_alphabet[(s+k)-len(rus_alphabet)])
             else:
                 res.append(rus_alphabet[s + k])
@@ -38,6 +42,23 @@ def decrypt(text = 'сдрс!', k = 1):
     res = ''.join(res)
     return res
 
-print(decrypt())
+def main():
+    global txt
+    key_ = 0
+    while True:
+        try:
 
+            txt = input("Введите текст для шифровки: ")
+            key_ = int(input("Введите сдвиг(число от 0 до 32): "))
 
+        except Exception:
+            print('Ошибка')
+        else:
+            print('Принято')
+            break
+    encrypt(text=txt,k=key_)
+    print("Зашифровано:",e_text)
+    pass
+
+if __name__ == '__main__':
+    main()
