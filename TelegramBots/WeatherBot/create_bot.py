0fe3210b0a2ef6b03787b8d59b6aedaac7041921
korvyakov_fn <enemy1987@yandex.ru>
2023-03-23 09:37:07 +0300

    new bot

---
+++ TelegramBots/WeatherBot/create_bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from config import TOKEN_BOT
+
+bot = Bot(TOKEN_BOT)
+dp = Dispatcher(bot)
---
+++ TelegramBots/WeatherBot/main.py
@@ -0,0 +1,25 @@
+from aiogram.utils import executor
+from create_bot import dp,bot
+from aiogram import types
+from weather import get_weather
+
+commands_list = '/map - получить положение города на карте\n' \
+                '/start - \n' \
+                '/list - получить список команд\n' \
+                '/description - описание бота\n' \
+                '/weather - текущая погода в городе\n' \
+                '/city - ввести город'
+
+async def on_startup(_):
+    print('Бот онлайн')
+
+@dp.message_handler(commands = ['start'])
+async def cmd_start(message:types.Message):
+    await message.answer('Напишите в каком городе узнать погоду?')
+
+@dp.message_handler()
+async def get_wtr(message: types.Message):
+    await message.reply( get_weather(message.text))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
