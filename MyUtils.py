c7f92d2be966563f8be9bdb6882e9bc2f6d1f1a5
kypena <ptl2022me@gmail.com>
2023-07-26 00:28:19 +0400

    help pull

--- MyUtils.py
+++ MyUtils.py
@@ -1,11 +1,11 @@
 import random
 
 
-def bubble_sort(my_list):
+def bubble_sort(my_list): # что значит def?
     last_item = len(my_list) - 1
     sor_list = []
     # print('Получен список: ', my_list)
-    for i in range(0, last_item):
+    for i in range(0, last_item): # что такое range?
         for j in range(0, last_item):
             if my_list[j] > my_list[j + 1]:
                 my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
@@ -13,7 +13,7 @@ def bubble_sort(my_list):
     # print("Отсортированный список: ", my_list)
 
 
-def uniq_list(a=0, b=100, ln=10):
+def uniq_list(a=0, b=100, ln=10):# что делает эта функция?
     list_ = []
     # TODO:"need tests"
 
